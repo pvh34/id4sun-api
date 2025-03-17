3e88a770764fd25426b62d354fdaffe6f2ed2585
pvh34 <hubertfelixth@gmail.com>
2025-03-17 14:17:21 +0100

    Add files via upload

--- calculs.py
+++ calculs.py
@@ -3,5 +3,5 @@ import numpy as np
 def calcul_tri(puissance, productible, taux_inflation, duree):
     # Ceci est un exemple fictif pour montrer comment structurer la fonction.
     # Tu remplaceras cette logique par celle de ton fichier Excel.
-    tri = (puissance * productible) / (1 + taux_inflation) ** duree
+    tri = (puissance * productible) / (1 + taux_inflation) 
     return tri
